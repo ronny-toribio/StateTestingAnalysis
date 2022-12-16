7953adf571f36e04119dbfac763a9a33f03d83d8
Ronny Toribio <rt48634@huskies.bloomu.edu>
2022-12-16 17:16:08 -0500

    New objectives in analysis.R

--- analysis.R
+++ analysis.R
@@ -10,6 +10,9 @@ library(GGally)
 ks = read_csv("Keystone/keystone.csv")
 ps = read_csv("PSSA/pssa.csv")
 
+# Objective 1: How our local districts in Columbia and Montour Counties are trending since 2015?
+
+# Objective 2: How they compare to the state trend since 2015?
 m = aov(Score ~ as_factor(Baseline) + as_factor(Subject) + Year, data = ks)
 summary(m)
 
@@ -19,14 +22,19 @@ summary(m1)
 plot(m)
 ggpairs(m)
 
-ks1 = read.csv("Keystone/keystone.csv")
-plot(ks1)
-
-
-# Objective 1: How our local districts in Columbia and Montour Counties are trending since 2015?
-
-# Objective 2: How they compare to the state trend since 2015?
-
 # Objective 3: Is there any COVID impact we might be able to deduce?
 
 # Objective 4: Any other information that data might tell us?
+
+# Objective 5: Visualizing the averages of scores from each year.
+
+# Objective 5a. As a whole.
+
+# Objective 5b. Grouped by subject.
+
+# Objective 5c. Grouped by district.
+
+# Objective 6. Compare scores between districts.
+
+# Objective 7. (John's objective)
+

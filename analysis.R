740e70a78700e49e066319393a319d5ad9fa124b
Ronny Toribio <rt48634@huskies.bloomu.edu>
2022-12-16 19:40:06 -0500

    Post merge clean up

--- analysis.R
+++ analysis.R
@@ -8,11 +8,9 @@ library(ggplot2)
 library(GGally)
 library(hrbrthemes)
 
-
 ks = read_csv("Keystone/keystone.csv")
 ps = read_csv("PSSA/pssa.csv")
 
-<<<<<<< HEAD
 # Objective 1: How our local districts in Columbia and Montour Counties are trending since 2015?
 
 # Objective 2: How they compare to the state trend since 2015?
@@ -24,20 +22,13 @@ summary(m1)
 
 plot(m)
 ggpairs(m)
-=======
 
-# Objective 1: How our local districts in Columbia and Montour Counties are trending since 2015?
-
-# Objective 2: How they compare to the state trend since 2015?
->>>>>>> refs/remotes/origin/main
 
 # Objective 3: Is there any COVID impact we might be able to deduce?
-
 m = aov(Score ~ as_factor(Subject) + Year + as_factor(Baseline) + 
           as_factor(District) + as_factor(School) + 
           as_factor(County), data = ks)
 summary(m)
-
 
 
 m2 = glm(Score ~ as_factor(Subject) + Year + as_factor(Baseline) + 
@@ -59,6 +50,7 @@ p1
 
 # Objective 4: Any other information that data might tell us?
 
+
 # Objective 5: Visualizing the averages of scores from each year.
 
 # Objective 5a. As a whole.
@@ -67,7 +59,9 @@ p1
 
 # Objective 5c. Grouped by district.
 
+
 # Objective 6. Compare scores between districts.
+
 
 # Objective 7. (John's objective)
 

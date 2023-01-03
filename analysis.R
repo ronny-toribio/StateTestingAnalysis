8c84514717c3e22d127c6df34a3127c3f30831e7
Kadir Altunel <kadiraltunel@Kadirs-MacBook-Air.local>
2023-01-02 21:58:06 -0500

    Code update and graphs

--- analysis.R
+++ analysis.R
@@ -34,11 +34,14 @@ summary(m)
 m1 = glm(Score ~ as_factor(Baseline) + as_factor(Subject) + Year, data = ks)
 summary(m1)
 
-png("Graphs/obj2-ks-m.png")
 plot(m)
-dev.off()
 ggpairs(m)
-ggsave("Graphs/obj2-ks-m-ggpairs.png")
+
+# We see that in general, Montour and Colombia county fared better than State 
+# since 2015 in terms of scores. Basic level showed a sharp increase in Montour
+# and Colombia counties in 2022. Proficient level also showed a sharp
+# increase especially in 2022 for Montour county
+
 
 # Objective 3: Is there any COVID impact we might be able to deduce?
 m = aov(Score ~ as_factor(Subject) + Year + as_factor(Baseline) + 
@@ -52,21 +55,14 @@ m2 = glm(Score ~ as_factor(Subject) + Year + as_factor(Baseline) +
            as_factor(County), data = ks)
 summary(m2)
 
-png("Graphs/obj3-ks-m.png")
+
 plot(m)
-dev.off()
 ggpairs(m)
-ggsave("Graphs/obj3-ks-m-ggpairs.png")
+
 
 m3 = glm(Score ~ as_factor(Baseline) + Year, data = ks)
 summary(m3)
 
-p1 = ggplot(data = ks, aes(x = Year, y = Score)) +
-  geom_bar(stat = "identity", fill = "blue") +
-  facet_wrap(~ as_factor(Baseline))
-ggsave("Graphs/obj3-ks-graph1.png")
-
-p1
 
 ks$County = as_factor(ks$County)
 str(ks$County)
@@ -74,34 +70,26 @@ levels(ks$County) = c("State", "Colombia", "Montour")
 levels(ks$County)
 
 
-p2 = ggplot(data = ks, aes(fill = as_factor(Baseline), y = Score, x = as_factor(County))) + 
+png(filename = "Obj3.png", width = 1280, height = 1280)
+
+p1 = ggplot(data = ks, aes(fill = as_factor(Baseline), y = Score, x = as_factor(County))) + 
   geom_bar(position="dodge", stat="identity") +
   facet_wrap(~Year) +
-  xlab("State vs Counties")
-ggsave("Graphs/obj3-ks-graph2.png")
-p2
+  xlab("State vs Counties") +
+  scale_fill_discrete(name = "Baseline")
 
-#BelowBasic: Colombia County outperformed State and Montour County prior to COVID.
-#After COVID, Colombia County suffered some against State Level. Montour County ranked the worst
-#throughout the years and reached all time low in 2021.
+p1
 
-#Basic: Colombia County outperformed both State and Montour County throughout the years.
-#After COVID, all levels experienced some losses. Montour County kept ranking the worst.
+dev.off()
 
-#Proficient: Once again Colombia County outperformed State and Montour County throughout the years.
-#Both Montour and Colombia Counties fared better compared to State levels. There was a 
-#some decline in Montour County in 2021 causing the score averages to reach to state levels.
-#However in 2022, there was a strong spike in Proficient in Montour and Colombia,
-#outpacing State
+# Checking score averages throughout the years for counties and subjects, it
+# is interesting to see that COVID didn't impact Math and English scores
+# dramatically for Colombia, Montour and State. In fact, there was a significant
+# improvement in the scores in 2022 compared to 2019 for math scores in Colombia
+# county. Math scores remained stable if not slightly improved for both state
+# and Montour. However, there was a steep decline in Science scores between 2021
+# and 2022 throughout the state which may be an indication of a COVID impact.
 
-#Advanced: Colombia County outperformed State and Montour County across the years.
-#Montour County performed slightly better than State. Once again, in 2021, there
-#was a slight delcine in scores possibly due to COVID
-
-#As graph suggests, all baseline levels experienced some decline in scores due to COVID
-#with one exception. As a matter of fact in 2021, there was a spike in Advanced Baseline which 
-#suggests that COVID was not a determinator for scores in this level.
-#Proficient level seemed to suffer the most due to COVID19 pandemic.
 
 # Objective 4: Any other information that data might tell us?
 
